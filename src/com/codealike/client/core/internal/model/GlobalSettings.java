931b057f4173c64c1a1d8162b87fcef94660a9d3
Daniel Iglesias <daniel.iglesias@outlook.com.ar>
2017-10-30 13:39:51 -0300

    If Json file has other fields, token is not recognized by IDE (fixes #51)

--- src/com/codealike/client/core/internal/model/GlobalSettings.java
+++ src/com/codealike/client/core/internal/model/GlobalSettings.java
@@ -1,5 +1,8 @@
 package com.codealike.client.core.internal.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GlobalSettings {
     private String userToken;
     private String apiUrl;
