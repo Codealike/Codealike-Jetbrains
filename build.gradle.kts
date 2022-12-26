9faa21e49a5079719400587913636f1649f8994e
Paulo Vitor Magacho <pvmagacho@gmail.com>
2022-12-26 12:14:18 -0300

    Removed libs from local folder

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,5 +43,22 @@ tasks {
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
+    implementation("jakarta.annotation:jakarta.annotation-api:2.1.0")
+    implementation("jakarta.inject:jakarta.inject-api:2.0.1.MR")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.14.0")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.14.0")
+    implementation("org.apache.httpcomponents:httpclient:4.5.14")
+    implementation("org.apache.httpcomponents:httpcore:4.4.16")
+    implementation("cglib:cglib:3.3.0")
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("org.glassfish.hk2:hk2:3.0.3")
+    implementation("org.glassfish.hk2:hk2-utils:3.0.3")
+    implementation("org.glassfish.jersey.core:jersey-client:3.1.0")
+    implementation("org.glassfish.jersey.core:jersey-common:3.1.0")
+    implementation("org.glassfish.jersey.inject:jersey-hk2:3.1.0")
+    implementation("log4j:log4j:1.2.17")
+    implementation("joda-time:joda-time:2.12.2")
+    implementation("nekohtml:nekohtml:1.9.6.2")
 }
